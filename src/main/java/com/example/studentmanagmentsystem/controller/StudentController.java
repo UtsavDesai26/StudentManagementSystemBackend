b507bb3e8d8a9a4c04bfd3f06fb67afb4b9218fd
Utsav Desai <20it026@charusat.edu.in>
2022-09-15 21:30:16 +0530

    4th commit

--- src/main/java/com/example/studentmanagmentsystem/controller/StudentController.java
+++ src/main/java/com/example/studentmanagmentsystem/controller/StudentController.java
@@ -1,32 +1,46 @@
 package com.example.studentmanagmentsystem.controller;
+
+import com.example.studentmanagmentsystem.model.Student;
+import com.example.studentmanagmentsystem.repository.StudentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 public class StudentController {
 
+    @Autowired
+    StudentRepository studentRepository;
 
     @GetMapping("/liststudent")
-    public String getAllStudent(){
-        return "Get All Student";
+    public List<Student> getAllStudent(){
+        return studentRepository.findAll();
     }
 
     @GetMapping("/student/{id}")
-    public String getstudent(){
-        return "Get";
+    public Student getstudent(@PathVariable Integer id){
+        return studentRepository.findById(id).get();
     }
 
     @PutMapping("/student/{id}")
-    public String updatestudent(){
-        return "Update";
+    public List<Student> updatestudent(@RequestBody Student student, @PathVariable Integer id){
+        Student studentobj = studentRepository.findById(id).get();
+        studentobj.setName(studentobj.getName());
+        studentobj.setAddress(studentobj.getAddress());
+        studentRepository.save(studentobj);
+        return studentRepository.findAll();
     }
 
     @DeleteMapping("/student/{id}")
-    public String deletestudent(){
-        return "Delete";
+    public List<Student> deletestudent(@PathVariable Integer id){
+        studentRepository.delete(studentRepository.findById(id).get());
+        return studentRepository.findAll();
     }
 
     @PostMapping("/student")
-    public String addstudent(){
-        return "Post";
+    public List<Student> addstudent(@RequestBody Student student){
+        studentRepository.save(student);
+        return studentRepository.findAll();
     }
 }
