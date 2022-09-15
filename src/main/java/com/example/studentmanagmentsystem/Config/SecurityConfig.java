330447b0ce19069525d40dfa5b7d4e90ee67b5df
Utsav Desai <20it026@charusat.edu.in>
2022-09-15 21:58:04 +0530

    6th commit

---
+++ src/main/java/com/example/studentmanagmentsystem/Config/SecurityConfig.java
@@ -0,0 +1,9 @@
+package com.example.studentmanagmentsystem.Config;
+
+
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+
+@EnableWebSecurity
+public class SecurityConfig {
+
+}
