dfa3f042e72fda63a7309f0ca3f6b0e5422ec9a9
Utsav Desai <20it026@charusat.edu.in>
2022-09-15 22:02:53 +0530

    7th commit

--- src/main/java/com/example/studentmanagmentsystem/Config/SecurityConfig.java
+++ src/main/java/com/example/studentmanagmentsystem/Config/SecurityConfig.java
@@ -2,8 +2,53 @@ package com.example.studentmanagmentsystem.Config;
 
 
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+
+import javax.sql.DataSource;
 
 @EnableWebSecurity
-public class SecurityConfig {
+public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
+    @Autowired
+    DataSource dataSource;
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+        auth.jdbcAuthentication().dataSource(dataSource)
+                .withDefaultSchema()
+                .withUser("admin")
+                .password("pass")
+                .roles("ADMIN", "USER")
+                .and()
+                .withUser("user")
+                .password("pass")
+                .roles("USER");
+
+
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("pass")
+                .roles("ADMIN", "USER")
+                .and()
+                .withUser("user")
+                .password("pass")
+                .roles("USER");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+
+    }
+
+    @Bean
+    public void getPasswordEncoder() {
+
+    }
 
 }
