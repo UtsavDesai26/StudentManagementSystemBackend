14c74fde723975e3d0e1814efac2f29d90936a79
Utsav Desai <20it026@charusat.edu.in>
2022-09-15 22:08:51 +0530

    8th commit

--- src/main/java/com/example/studentmanagmentsystem/Config/SecurityConfig.java
+++ src/main/java/com/example/studentmanagmentsystem/Config/SecurityConfig.java
@@ -1,54 +1,36 @@
 package com.example.studentmanagmentsystem.Config;
-
-
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 import javax.sql.DataSource;
 
+
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter{
+
 
     @Autowired
     DataSource dataSource;
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-
-        auth.jdbcAuthentication().dataSource(dataSource)
-                .withDefaultSchema()
-                .withUser("admin")
-                .password("pass")
-                .roles("ADMIN", "USER")
-                .and()
-                .withUser("user")
-                .password("pass")
-                .roles("USER");
-
-
-        auth.inMemoryAuthentication()
-                .withUser("admin")
-                .password("pass")
-                .roles("ADMIN", "USER")
-                .and()
-                .withUser("user")
-                .password("pass")
-                .roles("USER");
+        auth.jdbcAuthentication().dataSource(dataSource);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
-
+        http.csrf().disable();
+        http.headers().frameOptions().disable();
     }
 
     @Bean
-    public void getPasswordEncoder() {
-
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
-
 }
